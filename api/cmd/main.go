2872b4f1517a26d0dfc67c98984c06230ad14925
Mick <60408958+korn1k@users.noreply.github.com>
2024-09-13 18:28:27 -0400

    Modify to use router and set up server

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/korn1k/stripepayment/internal/config"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/korn1k/stripepayment/internal/config"
+	"github.com/korn1k/stripepayment/internal/router"
+	"github.com/stripe/stripe-go/v75"
+)
 
 func main() {
+	router := router.NewRouter()
+
 	c := config.MustInitConfig()
+	stripe.Key = c.Stripe.SecretKey
+
+	port := "8080"
+	fmt.Printf("Starting server on port %s...\n", port)
+	err := http.ListenAndServe(":"+port, router)
+	if err != nil {
+		fmt.Errorf("Error starting server: %v\n", err)
+	}
 }
